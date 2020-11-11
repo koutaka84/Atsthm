b80eb07edb01d4f63097e12b771c570647b5cd09
koutaka84 <giyama0810@gmail.com>
2020-11-11 11:47:32 +0000

    migreat Fix

--- db/migrate/20201027162502_add_followings_tousers.rb
+++ db/migrate/20201027162502_add_followings_tousers.rb
@@ -1,6 +1,5 @@
 class AddFollowingsTousers < ActiveRecord::Migration[5.2]
-  def changecreate_table :users do |t|
-      t.integer :followings
-
-    end
+  def change
+  	add_column :users, :followings, :integer
   end
+end
